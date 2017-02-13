a037bb828ca792fa41526d2c999a9f7d1bb99f2c
Ankit Agrawal <ankit.agrawal@causecode.com>
2017-02-13 15:27:05 +0530

    Added class level docs.

--- src/main/groovy/override/test/dummy/DummyService.groovy
+++ src/main/groovy/override/test/dummy/DummyService.groovy
@@ -1,5 +1,15 @@
 package override.test.dummy
 
+/**
+  * This interface declares the methods required for DefaultService.
+ *
+  * This plugin defines a DefaultService class which implements this interface and is registered as the
+  * dummyService bean.
+ *
+  * To override the default implementation of dummyService bean, the installing application should create a
+  * class(say CustomService) which implements this interface and finally inject your
+  * custom class (CustomService) as dummyService in resources.groovy.
+  */
 interface DummyService {
 
     def myMethod()
--- src/main/groovy/override/test/foo/DefaultService.groovy
+++ src/main/groovy/override/test/foo/DefaultService.groovy
@@ -2,6 +2,10 @@ package override.test.foo
 
 import override.test.dummy.DummyService
 
+/**
+  * This class is the default implementation of DummyService. This class is injected as dummyService bean.
+  * Check Plugin descriptor file (OverrideTestPluginGrailsPlugin.groovy) for bean override.
+  */
 class DefaultService implements DummyService {
 
     def myMethod() {
